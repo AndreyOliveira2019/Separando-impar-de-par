5a800bb4dfc571565ac96252057981f92b44a59a
AndreyOliveira2019 <55673289+AndreyOliveira2019@users.noreply.github.com>
2021-05-13 23:24:13 -0300

    Create code.py

---
+++ code.py
@@ -0,0 +1,6 @@
+lista = [6, 7, 4, 7, 8, 4, 2, 5, 7]
+for n in lista: 
+  if ((n%2) == 1):
+    print('Este resultado é impar', n)
+    
+#Se precisar separar apenas números pares da lista, o n%2 == 0
